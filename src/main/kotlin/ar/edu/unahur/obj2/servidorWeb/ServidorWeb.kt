9d1773d17a606aa029f538d28c9d127d04a21aa3
Pablo Araoz <pablodaniel_84@hotmail.com>
2021-10-21 22:31:28 -0300

    Completado hasta módulos inclusive

--- src/main/kotlin/ar/edu/unahur/obj2/servidorWeb/ServidorWeb.kt
+++ src/main/kotlin/ar/edu/unahur/obj2/servidorWeb/ServidorWeb.kt
@@ -1,6 +1,43 @@
 package ar.edu.unahur.obj2.servidorWeb
 
+import ar.edu.unahur.obj2.servidorWeb.integraciones.Consola
+
 object ServidorWeb {
-  fun procesar(pedido: PedidoHttp) =
-    RespuestaHttp(CodigoHttp.OK, "cualquier cosa", 200, pedido)
+  val modulos = mutableListOf<Modulo>()
+
+  fun procesar(pedido: PedidoHttp): RespuestaHttp {
+    if (obtenerProtocoloUrl(pedido.url) != "http") {
+      return RespuestaHttp(CodigoHttp.NOT_IMPLEMENTED,"", 10, pedido)
+    }
+    else {
+      return RespuestaHttp(CodigoHttp.OK, "cualquier cosa", 200, pedido)
+    }
+  }
+
+  fun obtenerProtocoloUrl(url: String) = url.substringBefore(":")
+
+  fun obtenerExtensionUrl(url: String) = url.substringAfterLast(".")
+
+  fun obtenerRutaUrl(url: String): String {
+    val ruta = url.split("/").subList(3,url.split("/").size)
+
+    return ruta.joinToString("/")
+  }
+
+  fun respuestaDelModulo(pedido: PedidoHttp): RespuestaHttp {
+    val moduloElegido = modulos.find{x -> x.extension.any{x -> x == obtenerExtensionUrl(pedido.url)}}
+    if (modulos.any{x -> x.extension.any{x -> x == obtenerExtensionUrl(pedido.url)}}) {
+      return RespuestaHttp(CodigoHttp.OK, moduloElegido!!.devuelve, moduloElegido!!.tiempo, pedido)
+    }
+    else {
+      return RespuestaHttp(CodigoHttp.NOT_FOUND, "", 10, pedido)
+    }
+  }
+
+  fun agregarModulo(unModulo: Modulo) { modulos.add(unModulo) }
+
+  fun removerModulo(unModulo: Modulo) { modulos.remove(unModulo) }
+
+  class Modulo(val extension: List<String>, val devuelve: String, val tiempo: Int)
+
 }
--- src/test/kotlin/ar/edu/unahur/obj2/servidorWeb/repl.ws.kts
+++ src/test/kotlin/ar/edu/unahur/obj2/servidorWeb/repl.ws.kts
@@ -5,3 +5,54 @@
 // en la corrección ni reemplaza a los tests.
 
 listOf(1, 8, 10).average()
+
+val url = "http://hola.com.ar/ruta/ruta2/ruta3/api.html"
+
+val url2 = url.split("/").subList(3,url.split("/").size)
+url2
+
+
+url2[0] + "/" + url2[1] + "/" + url2[2] + "/" + url2[3]
+url2.size
+
+url2.joinToString("/")
+
+
+/*var i = 0
+
+var msj = ""
+
+while (i < url2.size){
+    msj = msj + "/" + url2[i]
+    i += 1
+}*/
+
+//msj
+
+fun protocolo() = url.substringBefore(":")
+
+protocolo()
+
+
+fun extension() = url.substringAfterLast(".")
+
+extension()
+
+url.substringAfter(".ar")
+
+fun probar(num: Int) : Int{
+    var numerito = 0
+    if (num > 5){
+        numerito = 89
+    }
+    return numerito
+}
+
+probar(7)
+
+
+
+
+
+
+
